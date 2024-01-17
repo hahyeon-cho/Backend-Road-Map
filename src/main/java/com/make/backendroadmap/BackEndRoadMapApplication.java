5d47f1a8c6f32869319023e3af34e4656e1fd56d
JHyun0302 <wogustkfkd11@gmail.com>
2024-01-17 20:32:48 +0900

    :tada: [Init] Init Project

---
+++ src/main/java/com/make/backendroadmap/BackEndRoadMapApplication.java
@@ -0,0 +1,13 @@
+package com.make.backendroadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndRoadMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndRoadMapApplication.class, args);
+    }
+
+}
