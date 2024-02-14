5c541d44e453e055a29b07dee1581abebdcc5928
JHyun0302 <wogustkfkd11@gmail.com>
2024-02-14 15:38:21 +0900

    [Demo] OAuth 관련 테스트

--- src/main/java/com/make/backendroadmap/web/IndexController.java
+++ src/main/java/com/make/backendroadmap/web/IndexController.java
@@ -4,13 +4,11 @@ package com.make.backendroadmap.web;
 import com.make.backendroadmap.domain.security.auth.LoginUser;
 import com.make.backendroadmap.domain.security.auth.dto.SessionUser;
 import com.make.backendroadmap.domain.security.service.PostsService;
-import com.make.backendroadmap.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,27 +16,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
     private final PostsService postsService;
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
         log.info("call IndexController!");
-        
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
         return "index";
-    }
-
-    @GetMapping("/posts/save")
-    public String postsSave() {
-        return "posts-save";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable("id") Long id, Model model) {
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
-
-        return "posts-update";
     }
 }
