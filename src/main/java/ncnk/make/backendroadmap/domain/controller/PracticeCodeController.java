34ed44715c5480317afe6e5877bc8e4f2e050a84
JHyun0302 <wogustkfkd11@gmail.com>
2024-02-24 14:25:02 +0900

    :recycle: [Refactor] 웹 컴파일러 접속 시 로그인 필요

--- src/main/java/ncnk/make/backendroadmap/domain/controller/PracticeCodeController.java
+++ src/main/java/ncnk/make/backendroadmap/domain/controller/PracticeCodeController.java
@@ -1,29 +1,35 @@
 package ncnk.make.backendroadmap.domain.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ncnk.make.backendroadmap.domain.entity.Member;
+import ncnk.make.backendroadmap.domain.exception.SessionNullPointException;
+import ncnk.make.backendroadmap.domain.security.auth.dto.SessionUser;
+import ncnk.make.backendroadmap.domain.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/practice")
 public class PracticeCodeController {
+    private final MemberService memberService;
 
     @GetMapping()
-    public String webCompiler(Model model, HttpServletRequest request) {
-        //TODO 로그인 한 사용자를 조회해서 그 사람 id 값을 model에 담아서 반환해주기
-        HttpSession session = request.getSession();
+    public String webCompiler(Model model,
+                              @SessionAttribute(name = "member", required = false) SessionUser sessionUser) {
 
-        Member member = (Member) session.getAttribute("member");
+        if (sessionUser == null) {
+            throw new SessionNullPointException("[ERROR] SessionUser is null");
+        }
 
-        model.addAttribute("userID", 1);
+        Member member = memberService.findMemberByEmail(sessionUser.getEmail());
+        model.addAttribute("userID", member.getMemberId());
+
         return "ide";
     }
 
