5bb38036a09d43da48965bd50dffc76a298e055e
JHyun0302 <wogustkfkd11@gmail.com>
2024-03-02 12:03:56 +0900

    :bulb: [Todo] SolvedService - 코딩 테스트 포인트 추가, MemberService - 프로필 업데이트 관련 트랜잭션 time 추가

--- src/main/java/ncnk/make/backendroadmap/domain/service/SolvedService.java
+++ src/main/java/ncnk/make/backendroadmap/domain/service/SolvedService.java
@@ -2,6 +2,9 @@ package ncnk.make.backendroadmap.domain.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ncnk.make.backendroadmap.domain.aop.time.callback.TraceTemplate;
+import ncnk.make.backendroadmap.domain.entity.CodingTest;
+import ncnk.make.backendroadmap.domain.entity.Member;
 import ncnk.make.backendroadmap.domain.entity.Solved;
 import ncnk.make.backendroadmap.domain.repository.Solved.SolvedRepository;
 import org.springframework.data.domain.Page;
@@ -15,9 +18,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SolvedService {
     private final SolvedRepository solvedRepository;
+    private final TraceTemplate template;
 
-//    @Transactional
-//    public void calculatePoint(Member member, CodingTest codingTest) {
+    @Transactional
+    public void calculatePoint(Member member, CodingTest codingTest) {
 //        List<Solved> solvedByMemberAndCodingTest = solvedRepository.findSolvedByMemberAndCodingTest(member, codingTest);
 //        for (Solved solved : solvedByMemberAndCodingTest) {
 //            if (solved.getProblemSolved()) {
@@ -25,7 +29,7 @@ public class SolvedService {
 //                        codingTest.getProblemLevel()); //TODO: 리트코드 api의 레벨 정보가 hard/mid/easy 인지 상/중/하... 어떻게 들어오냐에 따라 변경해야함!
 //            }
 //        }
-//    }
+    }
 
     public Page<Solved> dynamicSearching(String difficulty, String order, Boolean problemSolved, Pageable pageable) {
         return solvedRepository.dynamicSearching(difficulty, order, problemSolved, pageable);
