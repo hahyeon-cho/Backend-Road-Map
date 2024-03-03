10da8aac8d90e2664e045791bcdc5cab9a8c498c
JHyun0302 <wogustkfkd11@gmail.com>
2024-03-03 11:42:03 +0900

    :bulb: [Comment] Exception

--- src/main/java/ncnk/make/backendroadmap/domain/exception/SessionNullPointException.java
+++ src/main/java/ncnk/make/backendroadmap/domain/exception/SessionNullPointException.java
@@ -1,5 +1,8 @@
 package ncnk.make.backendroadmap.domain.exception;
 
+/**
+ * 세션이 없을 경우 예외
+ */
 public class SessionNullPointException extends RuntimeException {
     public SessionNullPointException(String message) {
         super(message);
