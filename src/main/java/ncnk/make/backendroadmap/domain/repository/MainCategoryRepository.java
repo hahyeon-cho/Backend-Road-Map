bfd3bc9447a2827d46173475501c0994c18fa6f2
JHyun0302 <wogustkfkd11@gmail.com>
2024-03-03 11:23:31 +0900

    오타 수정

--- src/main/java/ncnk/make/backendroadmap/domain/repository/MainCategoryRepository.java
+++ src/main/java/ncnk/make/backendroadmap/domain/repository/MainCategoryRepository.java
@@ -6,7 +6,7 @@ import ncnk.make.backendroadmap.domain.entity.MainCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * 대분류 좋아요 테이블 Repository (Spring-data-JPA 이용)
+ * 대분류 테이블 Repository (Spring-data-JPA 이용)
  */
 public interface MainCategoryRepository extends JpaRepository<MainCategory, Long> {
     Optional<MainCategory> findMainCategoriesByMainDocsId(Long id); //대분류 PK를 통해 대분류 정보 반환
