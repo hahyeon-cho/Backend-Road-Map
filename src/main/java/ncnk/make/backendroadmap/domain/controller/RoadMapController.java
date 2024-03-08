e492fae56f0c807fa68814b2c16abd936b7ce51b
SemiHyeon <gustpal08@gmail.com>
2024-03-08 11:45:43 +0900

    navbar, footer 추가 로드맵 메인페이지 html 추가

--- src/main/java/ncnk/make/backendroadmap/domain/controller/RoadMapController.java
+++ src/main/java/ncnk/make/backendroadmap/domain/controller/RoadMapController.java
@@ -1,10 +1,18 @@
 package ncnk.make.backendroadmap.domain.controller;
 
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ncnk.make.backendroadmap.domain.entity.Member;
+import ncnk.make.backendroadmap.domain.exception.SessionNullPointException;
+import ncnk.make.backendroadmap.domain.security.auth.LoginUser;
+import ncnk.make.backendroadmap.domain.security.auth.dto.SessionUser;
+import ncnk.make.backendroadmap.domain.service.MemberService;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 /**
  * 로드맵 Controller
@@ -15,8 +23,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/roadmap")
 public class RoadMapController {
 
+    private final MemberService memberService;
+
     @GetMapping()
-    public String roadMapMain() {
+    public String roadMapMain(@LoginUser SessionUser user, Model model,
+                              @SessionAttribute(name = "member", required = false) SessionUser sessionUser){
+        if(sessionUser == null){
+            throw new SessionNullPointException("[ERROR] SessionUser is null");
+        }
+        Member member = memberService.findMemberByEmail(sessionUser.getEmail()); // 회원 검색
+        model.addAttribute("userID", member.getMemberId());
+        model.addAttribute("userPicture", user.getPicture());
         return "roadMap/roadMapMain";
     }
 
