d50cfe88f08d159ca6f304fa6d319685314d671f
JHyun0302 <wogustkfkd11@gmail.com>
2024-03-17 12:29:12 +0900

    :sparkles: [Feat] 코딩 테스트 페이지 및 문제 리스트 페이지 Controller & Repo

--- src/main/java/ncnk/make/backendroadmap/domain/repository/CodingTest/CodingTestRepository.java
+++ src/main/java/ncnk/make/backendroadmap/domain/repository/CodingTest/CodingTestRepository.java
@@ -1,10 +1,11 @@
 package ncnk.make.backendroadmap.domain.repository.CodingTest;
 
+import java.util.Optional;
 import ncnk.make.backendroadmap.domain.entity.CodingTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CodingTestRepository extends JpaRepository<CodingTest, Long>, CodingTestCustomRepository {
-
+    Optional<CodingTest> findCodingTestByCodingTestId(Long id);
 }
