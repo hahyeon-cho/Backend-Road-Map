2c6fe15d6c3b4c80aef789a296179bba30275a65
SemiHyeon <gustpal08@gmail.com>
2024-03-21 17:52:28 +0900

    practice 페이지 생성및 css 작성 practice 페이지 controller 연결

--- src/main/java/ncnk/make/backendroadmap/domain/controller/PracticeCodeController.java
+++ src/main/java/ncnk/make/backendroadmap/domain/controller/PracticeCodeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ncnk.make.backendroadmap.domain.entity.Member;
 import ncnk.make.backendroadmap.domain.exception.SessionNullPointException;
+import ncnk.make.backendroadmap.domain.security.auth.LoginUser;
 import ncnk.make.backendroadmap.domain.security.auth.dto.SessionUser;
 import ncnk.make.backendroadmap.domain.service.MemberService;
 import org.springframework.stereotype.Controller;
@@ -23,18 +24,23 @@ public class PracticeCodeController {
     private final MemberService memberService;
 
     @GetMapping()
-    public String webCompiler(Model model,
+    public String webCompiler(@LoginUser SessionUser user, Model model,
                               @SessionAttribute(name = "member", required = false) SessionUser sessionUser) {
 
         //로그인 하지 않은 사용자 접근 불가
         if (sessionUser == null) {
             throw new SessionNullPointException("[ERROR] SessionUser is null");
         }
-
         Member member = memberService.findMemberByEmail(sessionUser.getEmail()); //회원 찾기
         model.addAttribute("userID", member.getMemberId()); //회원 PK값 model에 담고
+        model.addAttribute("userPicture", user.getPicture());
 
-        return "ide"; //ide.html로 반환
+        return "navi/practice";
+    }
+    @GetMapping("/ide")
+    public String wevCompilerIde() {
+    //ide.html로 반환
+        return "ide";
     }
 
 
