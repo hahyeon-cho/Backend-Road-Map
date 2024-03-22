05759514df3635a9c3d3db44b3bf08373c7fa416
hyoseon <kimhyoseon928@gmail.com>
2024-03-22 17:50:36 +0900

    [Feat] Quiz 채점 로직
    - 문제 과반수 맞을 경우 성공

--- src/main/java/ncnk/make/backendroadmap/domain/restController/QuizApiController.java
+++ src/main/java/ncnk/make/backendroadmap/domain/restController/QuizApiController.java
@@ -16,10 +16,7 @@ import ncnk.make.backendroadmap.domain.restController.dto.Quiz.QuizResponseDto;
 import ncnk.make.backendroadmap.domain.service.CodingTestService;
 import ncnk.make.backendroadmap.domain.service.MainCategoryService;
 import ncnk.make.backendroadmap.domain.service.QuizService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 퀴즈 RestController (json)
