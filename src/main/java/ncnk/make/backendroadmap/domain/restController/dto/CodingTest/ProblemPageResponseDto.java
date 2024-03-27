8b7fb02b4e6fd401cf427d23c1d9cd6c912e7907
JHyun0302 <wogustkfkd11@gmail.com>
2024-03-27 12:09:25 +0900

    :sparkles: [Feat] 코딩 테스트 문제 페이지에서 Status 기능 추가 ("푼 문제"/"안 푼 문제"/"틀린 문제")

--- src/main/java/ncnk/make/backendroadmap/domain/restController/dto/CodingTest/ProblemPageResponseDto.java
+++ src/main/java/ncnk/make/backendroadmap/domain/restController/dto/CodingTest/ProblemPageResponseDto.java
@@ -2,20 +2,32 @@ package ncnk.make.backendroadmap.domain.restController.dto.CodingTest;
 
 import lombok.Getter;
 import ncnk.make.backendroadmap.domain.entity.CodingTest;
+import ncnk.make.backendroadmap.domain.entity.Solved;
 
 @Getter
 public class ProblemPageResponseDto {
     private String problemTitle;
     private String problemLevel;
     private Double problemAccuracy;
+    private String isSolved;
 
-    private ProblemPageResponseDto(CodingTest codingTest) {
+    private ProblemPageResponseDto(CodingTest codingTest, Solved solved) {
         this.problemTitle = codingTest.getProblemTitle();
         this.problemLevel = codingTest.getProblemLevel();
         this.problemAccuracy = codingTest.getProblemAccuracy();
+        this.isSolved = changeToString(solved);
     }
 
-    public static ProblemPageResponseDto createCodingTestResponse(CodingTest codingTest) {
-        return new ProblemPageResponseDto(codingTest);
+    public static ProblemPageResponseDto createCodingTestResponse(CodingTest codingTest, Solved solved) {
+        return new ProblemPageResponseDto(codingTest, solved);
+    }
+
+    public String changeToString(Solved solved) {
+        if (solved != null) {
+            Boolean problemSolved = solved.getProblemSolved();
+            return String.valueOf(problemSolved);
+        } else {
+            return "unsolved";
+        }
     }
 }
