42b0f35d62154d59a4161483a413510307d40def
JHyun0302 <wogustkfkd11@gmail.com>
2024-03-31 10:48:12 +0900

    :recycle: [Refactor] 코딩 테스트 페이지에 사용자 사진 추가

--- src/main/java/ncnk/make/backendroadmap/domain/controller/CodingTestController.java
+++ src/main/java/ncnk/make/backendroadmap/domain/controller/CodingTestController.java
@@ -1,9 +1,11 @@
 package ncnk.make.backendroadmap.domain.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ncnk.make.backendroadmap.domain.entity.CodingTest;
 import ncnk.make.backendroadmap.domain.entity.Member;
+import ncnk.make.backendroadmap.domain.exception.SessionNullPointException;
 import ncnk.make.backendroadmap.domain.security.auth.LoginUser;
 import ncnk.make.backendroadmap.domain.security.auth.dto.SessionUser;
 import ncnk.make.backendroadmap.domain.service.CodingTestService;
@@ -12,15 +14,12 @@ import ncnk.make.backendroadmap.domain.service.SolvedService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.ui.Model;
-
-import java.util.List;
 
 /**
  * 코딩 테스트 페이지
@@ -36,26 +35,33 @@ public class CodingTestController {
 
     // 문제 리스트 페이지
     @GetMapping("")
-    public String codingTest(Model model) {
+    public String codingTest(@LoginUser SessionUser user, Model model) {
+        loginValidate(user);
+//        Member member = memberService.findMemberByEmail(user.getEmail());
+
         //TODO: templates/codingTest/codingTest.html 연결하기
         List<CodingTest> problems = codingTestService.findAllProblems();
+        model.addAttribute("userPicture", user.getPicture());
         model.addAttribute("problems", problems);
         return "/codingTest/codingTest";
     }
 
     @GetMapping("/{id}")
-    public String problemDetail(@PathVariable("id") Long codingTestId, Model model) {
+    public String problemDetail(@PathVariable("id") Long codingTestId, @LoginUser SessionUser user, Model model) {
+        loginValidate(user);
+//        Member member = memberService.findMemberByEmail(user.getEmail());
+        model.addAttribute("userPicture", user.getPicture());
+
         CodingTest problem = codingTestService.findCodingTestById(codingTestId);
         model.addAttribute("problemId", problem.getCodingTestId());
         model.addAttribute("problemTitle", problem.getProblemTitle());
         model.addAttribute("problemDescription", problem.getProblemContents());
         model.addAttribute("problemLevel", problem.getProblemLevel());
-        if(problem.getProblemTopics() != null) {
+        if (problem.getProblemTopics() != null) {
             model.addAttribute("problemTopic", String.join(", ", problem.getProblemTopics()));
         }
         return "/codingTest/codingTestProblem";
     }
-
 
 
     // 제출 버튼
@@ -79,6 +85,12 @@ public class CodingTestController {
             return ResponseEntity.ok().body("{\"result\":\"correct\"}");
         } else {
             return ResponseEntity.ok().body("{\"result\":\"incorrect\"}");
+        }
+    }
+
+    private static void loginValidate(SessionUser user) {
+        if (user == null) {
+            throw new SessionNullPointException("[ERROR] SessionUser is null");
         }
     }
 }
